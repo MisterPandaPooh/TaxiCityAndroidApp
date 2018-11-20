a896acdf8c145e145a1d045db8a7d94cb81da498
nanourafougithub <ncohenso@g.jct.ac.il>
2018-11-20 23:23:41 +0200

    Ajout du UI et remaniment de l'interface graphique

---
+++ app/src/main/java/taxicity/com/taxicityapp/controller/activities/MainActivity.java
@@ -0,0 +1,50 @@
+package taxicity.com.taxicityapp.controller.activities;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import taxicity.com.taxicityapp.R;
+import taxicity.com.taxicityapp.controller.fragments.RegisterFragment;
+
+public class MainActivity extends AppCompatActivity implements RegisterFragment.OnFragmentInteractionListener {
+
+    private RegisterFragment fragment = null;
+    private Button btnOrder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOrder =  findViewById(R.id.btn_get_taxi);
+
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                configureRegisterFragement();
+            }
+        });
+
+    }
+
+    private void configureRegisterFragement() {
+        if (fragment == null) { //Singleton of the fragement
+            fragment = new RegisterFragment();
+        }
+        btnOrder.setVisibility(View.INVISIBLE);
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame_layout, fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/taxicity/com/taxicityapp/controller/activities/OrderTrip.java
@@ -0,0 +1,15 @@
+package taxicity.com.taxicityapp.controller.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import taxicity.com.taxicityapp.R;
+
+public class OrderTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_trip);
+    }
+}
