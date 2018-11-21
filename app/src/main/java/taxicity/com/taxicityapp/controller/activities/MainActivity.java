ac6e9ba447e07f33d8df373123737b3ceedf3ace
nanourafougithub <ncohenso@g.jct.ac.il>
2018-11-21 13:15:11 +0200

    Ajout des Commentaire et Clarification du code

--- app/src/main/java/taxicity/com/taxicityapp/controller/activities/MainActivity.java
+++ app/src/main/java/taxicity/com/taxicityapp/controller/activities/MainActivity.java
@@ -16,8 +16,16 @@ import taxicity.com.taxicityapp.controller.fragments.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity implements RegisterFragment.OnFragmentInteractionListener {
 
+    /**
+     * The Fragment who is displaying the request of trip form.
+     */
     private RegisterFragment fragment = null;
+
+    /**
+     * The button who is starting the App
+     */
     private Button btnOrder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements RegisterFragment.
 
         btnOrder = findViewById(R.id.btn_get_taxi);
 
+        //Init Event Listener
         btnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,14 +44,21 @@ public class MainActivity extends AppCompatActivity implements RegisterFragment.
 
     }
 
-    //TODO se debrouiller pour que le bouton ne se remet pas
+    //TODO se debrouiller pour que le bouton ne se remet pas (Peut etre le reset de l'activity).
 
+    /**
+     * Configure and Init the "Trip Request" Fragment
+     */
     private void configureRegisterFragement() {
 
-        if (fragment == null) { //Singleton of the fragement
+        //Singleton pattern of the fragment.
+        if (fragment == null)
             fragment = new RegisterFragment();
-        }
+
+        //Hide the Button when displaying the fragment.
         btnOrder.setVisibility(View.GONE);
+
+        //Adding the Register fragment to the view.
         getSupportFragmentManager().beginTransaction().add(R.id.main_frame_layout, fragment).commit();
     }
 
